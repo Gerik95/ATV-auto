6c275be4db45b1958c0a8ba91c0a1791fed1280d
GerikPC <dimagerasimenko95@gmail.com>
2024-03-22 19:28:08 +0300

    email sending bugfix

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,7 +1,9 @@
-import React, {useRef, useState} from 'react';
-import {EuiButton, EuiFieldText, EuiForm, EuiFormRow,} from '@elastic/eui';
+import React, {useEffect, useRef, useState} from 'react';
+import {EuiButton, EuiFieldText, EuiForm, EuiFormRow, EuiTextArea, EuiFieldNumber} from '@elastic/eui';
 import emailjs from '@emailjs/browser';
 import {ToastContainer, toast} from 'react-toastify';
+
+import {object, string, number, date, InferType} from 'yup';
 
 import 'react-toastify/dist/ReactToastify.css';
 import './Form.css';
@@ -17,31 +19,40 @@ const fields = [
     {
         id: 1,
         name: 'vinCode',
-        placeholder: "VIN-код",
+        placeholder: "* VIN-код",
+        component: EuiFieldText,
     },
     {
         id: 2,
         name: 'name',
-        placeholder: "Ваше имя",
+        placeholder: "* Ваше имя",
+        component: EuiFieldText,
     },
     {
         id: 3,
         name: 'phone',
-        placeholder: "Телефон",
+        placeholder: "* Телефон",
+        component: EuiFieldNumber,
     },
     {
         id: 4,
         name: 'mail',
         placeholder: "Электронная почта",
+        component: EuiFieldText,
     },
     {
         id: 5,
         name: 'note',
-        placeholder: "Введите название запчасти",
+        placeholder: "* Введите название запчасти",
+        component: EuiTextArea,
     },
 ]
+
+
 const Form = () => {
     const [fieldsValue, setFieldsValue] = useState(defaultFieldsValue);
+    const [errors, setErrors] = useState({});
+
     const notifySuccess = () => toast.success('VIN-запрос успешно отправлен', {
         position: "top-right",
         autoClose: 5000,
@@ -64,19 +75,51 @@ const Form = () => {
     });
     const form = useRef();
 
-    const onChange = event => {
-        const {name, value} = event.target;
+    const generateErrors = () => {
+        let errorsList = [];
+        let test = {};
+        const EMAIL_REGEXP = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
+
+        if (fieldsValue.vinCode.length < 17) {
+            test = {...test, vinCode: 'VIN-код должен содержать 17 символов'}
+            errorsList.push('VIN-код должен содержать 17 символов')
+        }
+        if (!fieldsValue.name.length) {
+            test = {...test, name: 'Это поле является обязательным'}
+            errorsList.push('Это поле является обязательным')
+        }
+        if (!fieldsValue.phone.length) {
+            test = {...test, phone: 'Это поле является обязательным'}
+            errorsList.push('Это поле является обязательным')
+        }
+        if (!EMAIL_REGEXP.test(fieldsValue.mail) && fieldsValue.mail.length) {
+            test = {...test, mail: 'Адрес эл.почты введён не верно'}
+            errorsList.push('Адрес эл.почты введён не верно')
+        }
+        if (!fieldsValue.note.length) {
+            test = {...test, note: 'Это поле является обязательным'}
+            errorsList.push('Это поле является обязательным')
+        }
+        setErrors(test)
+        return errorsList
+    }
+    const onChange = (event, name) => {
+        const {value} = event.target;
         setFieldsValue({...fieldsValue, [name]: value})
     }
 
     const submit = async (event) => {
         event.preventDefault()
+        if (generateErrors().length) {
+            return
+        }
         try {
             await emailjs
                 .sendForm('service_q84tp5l', 'template_jy4qcy7', form.current, {
                     publicKey: 'YH19HLxP68P3cWIiY',
                 })
             setFieldsValue(defaultFieldsValue);
+            setErrors({});
             notifySuccess();
 
         } catch (error) {
@@ -85,12 +128,42 @@ const Form = () => {
         }
     }
 
+    const input = (item, onChange) => {
+        switch (item.component) {
+            case EuiFieldText:
+                return (
+                    <>
+                        <EuiFieldText value={item.value} name={item.name} onChange={(event) => onChange(event, item.name)}
+                                      placeholder={item.placeholder}/>
+                        {errors[item.name] && <p className='message-error'>{errors[item.name]}</p>}
+                    </>
+                )
+            case EuiFieldNumber:
+                return (
+                    <>
+                        <EuiFieldNumber value={item.value} name={item.name} onChange={(event) => onChange(event, item.name)}
+                                        placeholder={item.placeholder}/>
+                        {errors[item.name] && <p className='message-error'>{errors[item.name]}</p>}
+                    </>
+                )
+            case EuiTextArea:
+                return (
+                    <>
+                        <EuiTextArea value={item.value} name={item.name} onChange={(event) => onChange(event, item.name)}
+                                     placeholder={item.placeholder}/>
+                        {errors[item.name] && <p className='message-error'>{errors[item.name]}</p>}
+                    </>
+                )
+            default:
+                return <></>;
+        }
+    }
+
     return (
         <EuiForm component="form" onSubmit={submit} ref={form}>
             {fields.map(field => (
                 <EuiFormRow key={field.id}>
-                    <EuiFieldText fullWidth name={field.name} placeholder={field.placeholder}
-                                  value={fieldsValue[field.name]} onChange={onChange}/>
+                    {input(field, onChange)}
                 </EuiFormRow>
             ))}
             <EuiButton style={{backgroundColor: '#fc8b06'}} type="submit" fill>
